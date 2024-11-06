0ad3228f39d3e7bf2155a204bb7db8a0abd598bc
valentinabc19 <valentina.bueno_col@uao.edu.co>
2024-11-06 13:01:15 -0500

    Removing graficas microservice

--- docker_compose/microservicios/graficas/src/models/profesoresModel.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def obtener_correo_profesor(usuario):
-    url = f'http://192.168.100.2:3005/correoprof/{usuario}'
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return None
